adedc35e3e9d36b173680739eb808874b79cd668
robson10mateus <68832913+robson10mateus@users.noreply.github.com>
2023-03-21 11:21:09 -0300

    Inferface nova

--- Weather/script.js
+++ Weather/script.js
@@ -5,6 +5,7 @@ const temperatureEl = document.getElementById('temperature');
 const weatherEl = document.getElementById('weather');
 const body = document.querySelector('body');
 const locationBtn = document.getElementById('location-btn');
+const imagem = document.getElementById('img');
 
 searchBtn.addEventListener('click', () => {
 	const cityName = cityInput.value;
@@ -17,6 +18,9 @@ searchBtn.addEventListener('click', () => {
 			locationEl.textContent = `Localização: ${data.name}, ${data.sys.country}`;
 			temperatureEl.textContent = `Temperatura: ${data.main.temp}°C`;
 			weatherEl.textContent = `Clima atual: ${data.weather[0].description}`;
+
+			img.src = `C:/Users/ROBSON/Desktop/Weather/src/${data.weather[0].icon}.png`;
+
 			const weather = data.weather[0].main.toLowerCase();
 			body.classList = '';
 			body.classList.add(weather);
@@ -38,6 +42,9 @@ locationBtn.addEventListener('click', () => {
 					locationEl.textContent = `Localização: ${data.name}, ${data.sys.country}`;
 					temperatureEl.textContent = `Temperatura: ${data.main.temp}°C`;
 					weatherEl.textContent = `Clima atual: ${data.weather[0].description}`;
+
+					img.src = `C:/Users/ROBSON/Desktop/Weather/src/${data.weather[0].icon}.png`;
+
 					const weather = data.weather[0].main.toLowerCase();
 					body.classList = '';
 					body.classList.add(weather);
